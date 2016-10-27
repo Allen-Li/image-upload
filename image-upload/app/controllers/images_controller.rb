f00905f850eaef549dbd6f60984e1a7b821adb43
evan <evli@xogrp.com>
2016-10-27 17:36:40 +0800

    upload image to local server

---
+++ image-upload/app/controllers/images_controller.rb
@@ -0,0 +1,25 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new image_attributes
+    if @image.save
+      redirect_to image_path
+    else
+      flash[:error] =  @image.errors.messages
+      render action: 'new'
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_attributes
+    params.require(:image).permit(:asset, :html)
+  end
+end
