4dca92087679798569339236caaa2e907da2a956
evan <evli@xogrp.com>
2016-11-08 14:35:19 +0800

    render image

--- image-upload/app/controllers/images_controller.rb
+++ image-upload/app/controllers/images_controller.rb
@@ -10,7 +10,7 @@ class ImagesController < ApplicationController
   def create
     @image = Image.new image_attributes
     if @image.save
-      redirect_to image_path
+      redirect_to image_path @image
     else
       flash[:error] =  @image.errors.messages
       render action: 'new'
@@ -19,6 +19,19 @@ class ImagesController < ApplicationController
 
   def show
     @image = Image.find(params[:id])
+  end
+
+  def preview_html
+    image = Image.find(params[:image_id])
+    image_url = image.asset.url
+    image_url_el = "<img src='#{image_url}'></img>"
+    html = image.html
+    if html.present?
+      html.gsub!('{{#content}}', image_url_el)
+    else
+      html = image_url_el
+    end
+    render html: html.html_safe
   end
 
   private
--- image-upload/config/routes.rb
+++ image-upload/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :images
+
+  resources :images do
+    get 'preview_html' => 'images#preview_html'
+  end
 end
